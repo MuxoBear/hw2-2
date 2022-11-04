93d1e10228f3979455cde92e7a90c8893e301f83
MuxoBear <semargla2@mail.ru>
2022-11-04 19:41:25 +0700

    hw2-2

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,20 @@ public class Main {
         System.out.println("Разница в весе " + weightDifference + "кг. ");
         var preponderance = boxer2 % boxer1;
         System.out.println("Второй боксер тяжилее первого на " + preponderance + " кг.");
+        var totalWorkingHours = 640;
+        System.out.println("Общее рабочее время в компании " + totalWorkingHours + " часов");
+        var workingHoursOfOneEmployee = 8;
+        System.out.println("Рабочее время одного сотрудника " + workingHoursOfOneEmployee + " часов");
+        var numberOfEmployees = totalWorkingHours / workingHoursOfOneEmployee;
+        System.out.println("Общее число сотрудников в компании " + numberOfEmployees + " человек.");
+        var newEmployees = 94;
+        System.out.println("Новых сотрудников " + newEmployees + " человека.");
+        var newTotalNumberOfEmployees = numberOfEmployees + newEmployees;
+        System.out.println("Новое количество сотрудников " + newTotalNumberOfEmployees + " человека.");
+        var newTotalWorkingHours = newTotalNumberOfEmployees * workingHoursOfOneEmployee;
+        System.out.println("Общее количество рабочих часов в компании " + newTotalWorkingHours + " часов.");
+        var workingHours = newTotalWorkingHours / newTotalNumberOfEmployees;
+        System.out.println("Если в компании работает всего 174 человека, то всего 1392 часа может быть поделено между сотрудниками. В итоге получим  " + workingHours + " часов на одного человека");
 
     }
 }
