fa200c82dcc130070cc7704e44e07a6187632b73
MuxoBear <semargla2@mail.ru>
2022-11-04 19:05:36 +0700

    hw2-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Боксеры");
+        var boxer1 = 78.2;
+        System.out.println("Первый боксер " + boxer1 + "кг. ");
+        var boxer2 = 82.7;
+        System.out.println("Второй боксер " + boxer2 + "кг. ");
+        var totalWeight = boxer1 + boxer2;
+        System.out.println("Общий вес " + totalWeight + "кг. ");
+        var weightDifference = boxer2 - boxer1;
+        System.out.println("Разница в весе " + weightDifference + "кг. ");
+        var preponderance = boxer2 % boxer1;
+        System.out.println("Второй боксер тяжилее первого на " + preponderance + " кг.");
+
     }
 }
